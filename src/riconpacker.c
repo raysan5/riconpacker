1bb219d6be66f06b791deaf8075a230f7e2459d2
Ray <raysan5@gmail.com>
2023-05-10 00:30:56 +0200

    Updated to `raygui 3.6`

--- src/riconpacker.c
+++ src/riconpacker.c
@@ -47,7 +47,7 @@
 *
 *   DEPENDENCIES:
 *       raylib 4.2              - Windowing/input management and drawing
-*       raygui 3.5              - Immediate-mode GUI controls with custom styling and icons
+*       raygui 3.6              - Immediate-mode GUI controls with custom styling and icons
 *       rpng 1.0                - PNG chunks management
 *       tinyfiledialogs 3.9.0   - Open/save file dialogs, it requires linkage with comdlg32 and ole32 libs
 *       miniz 2.2.0             - Save .zip package file (required for multiple images export)
